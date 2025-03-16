f76942b1deb45b7449546fbf255c692ab52fc626
phanvantien2907 <vantienp530@gmail.com>
2025-03-16 16:35:27 +0700

    Render menu page

--- src/app/(menu)/[slug]/page.tsx
+++ src/app/(menu)/[slug]/page.tsx
@@ -3,14 +3,41 @@ import { useParams } from 'next/navigation'
 import CustomBreadcrumb from '@/app/components/CustomBreadcrumb/CustomBreadcrumb';
 import ContactPage from '../components/ContactPage/ContactPage';
 import AboutPage from '../components/AboutPage/AboutPage';
+import axios from "axios";
+import { useState, useEffect } from 'react';
+
+interface menuItem {
+  id: string;
+  name: string;
+  url: string;
+}
 
 const Menupage = () => {
+    const [menuData, setMenuData] = useState<menuItem[]>([]);
     const {slug} = useParams()
+
+    useEffect(() => {
+      axios.get('http://127.0.0.1:8000/api/menu')
+        .then((res) => setMenuData(res.data.data))
+        .catch((err) => console.error("Lỗi gọi API:", err));
+    }, []);
+
+    // hàm render menu dựa trên slug 
+    const renderMenu = () => {
+     switch(slug) {
+      case "lien-he":
+      return <ContactPage />;
+      case "about-laluz":
+        return <AboutPage />;
+        default:
+        return <p>Không tìm thấy trang</p>;
+    }
+    };
+
   return (
     <div>
     <CustomBreadcrumb />
-    {/* <ContactPage /> */}
-    <AboutPage />
+    {renderMenu()}
     </div>
   )
 }
